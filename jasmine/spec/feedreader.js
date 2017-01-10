c04384076231b747d8110aed810b02070269f594
CHIRAG CHOPRA <chiragchopra7@gmail.com>
2017-01-10 09:44:18 +0530

    recovered error

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -86,7 +86,7 @@ $(function() {
                     loadFeed(0, function() {
             feed = $('.header-title').html();
             loadFeed(1, function(){ //nested load2 to meet async function demands
-                 $newfeed = $('.header-title').html();
+                 newfeed = $('.header-title').html();
                  done();
             });
          });
